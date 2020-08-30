ad4ba90c12bb461822f86402728f22b954ec40f9
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-30 23:40:42 +0700

    50

--- butterfly_pattern/Source.cpp
+++ butterfly_pattern/Source.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h>
-struct number {
-    int x[10];
+struct PATTERN {
+    int fa[10];
     int i;
-    int j;
+    int q;
 }num, row, star, space;
 
 
-void moretriangle(int n)
+void Moretriangle(int n)
 {
     for (row.i = 1; row.i <= n - 1 && row.i > 0; row.i++)
     {
-        for (star.j = 1; star.j <= row.i && star.j > 0; star.j++)
+        for (star.q = 1; star.q <= row.i && star.q > 0; star.q++)
         {
             printf("* ");
         }
-        for (space.j = 1; space.j < (2 * n) - (row.i * 2) && space.j>0; space.j++)
+        for (space.q = 1; space.q < (2 * n) - (row.i * 2) && space.q>0; space.q++)
         {
             printf("  ");
         }
-        for (star.j = 1; star.j <= (n * 2) - 1 && star.j > 0; star.j++)
+        for (star.q = 1; star.q <= (n * 2) - 1 && star.q > 0; star.q++)
         {
             printf("* ");
-            if (star.j == row.i) break;
+            if (star.q == row.i) break;
         }
         printf("\n");
     }
@@ -38,22 +38,22 @@ void triangle(int n)
 }
 
 
-void lasstriangle(int n)
+void pATTERN(int n)
 {
     for (row.i = 1; row.i <= n - 1 && row.i > 0; row.i++)
     {
-        for (star.j = n - 1; star.j >= row.i; star.j--)
+        for (star.q = n - 1; star.q >= row.i; star.q--)
         {
             printf("* ");
         }
-        for (space.j = 1; space.j < row.i * 2 && space.j>0; space.j++)
+        for (space.q = 1; space.q < row.i * 2 && space.q>0; space.q++)
         {
             printf("  ");
         }
-        for (star.j = n - 1; star.j > 0; star.j--)
+        for (star.q = n - 1; star.q > 0; star.q--)
         {
             printf("* ");
-            if (star.j == row.i) break;
+            if (star.q == row.i) break;
         }
         printf("\n");
     }
@@ -62,9 +62,10 @@ void lasstriangle(int n)
 
 int main()
 {
-    scanf("%d", &num.x[1]);
-    moretriangle(num.x[1]);
-    triangle(num.x[1]);
-    lasstriangle(num.x[1]);
+    printf
+    scanf_s("%d", &num.fa[1]);
+    Moretriangle(num.fa[1]);
+    triangle(num.fa[1]);
+    pATTERN(num.fa[1]);
     return 0;
 }
