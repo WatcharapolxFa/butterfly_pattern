2692e5d0a303ad7a57e78a7594296719079ca8c7
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-30 23:44:45 +0700

    100 % fa

--- butterfly_pattern/Source.cpp
+++ butterfly_pattern/Source.cpp
@@ -5,39 +5,6 @@ struct PATTERN {
     int q;
 }num, row, star, space;
 
-
-void Moretriangle(int n)
-{
-    for (row.i = 1; row.i <= n - 1 && row.i > 0; row.i++)
-    {
-        for (star.q = 1; star.q <= row.i && star.q > 0; star.q++)
-        {
-            printf("* ");
-        }
-        for (space.q = 1; space.q < (2 * n) - (row.i * 2) && space.q>0; space.q++)
-        {
-            printf("  ");
-        }
-        for (star.q = 1; star.q <= (n * 2) - 1 && star.q > 0; star.q++)
-        {
-            printf("* ");
-            if (star.q == row.i) break;
-        }
-        printf("\n");
-    }
-}
-
-
-void triangle(int n)
-{
-    for (row.i = 1; row.i <= (2 * n) - 1 && row.i > 0; row.i++)
-    {
-        printf("* ");
-    }
-    printf("\n");
-}
-
-
 void pATTERN(int n)
 {
     for (row.i = 1; row.i <= n - 1 && row.i > 0; row.i++)
@@ -60,12 +27,55 @@ void pATTERN(int n)
 }
 
 
+
+void triangle(int n)
+{
+    for (row.i = 1; row.i <= (2 * n) - 1 && row.i > 0; row.i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
+
+
+
+void Moretriangle(int n)
+{
+    for (row.i = 1; row.i <= n - 1 && row.i > 0; row.i++)
+    {
+        for (star.q = 1; star.q <= row.i && star.q > 0; star.q++)
+        {
+            printf("* ");
+        }
+        for (space.q = 1; space.q < (2 * n) - (row.i * 2) && space.q>0; space.q++)
+        {
+            printf("  ");
+        }
+        for (star.q = 1; star.q <= (n * 2) - 1 && star.q > 0; star.q++)
+        {
+            printf("* ");
+            if (star.q == row.i) break;
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf
-    scanf_s("%d", &num.fa[1]);
+
+    printf("Enter number : ");
+    if (scanf_s("%d", &num.fa[1]))
+    {
     Moretriangle(num.fa[1]);
     triangle(num.fa[1]);
     pATTERN(num.fa[1]);
+    printf("End Program.");
+    }
+    else
+    {
+        printf("IN PUT Integer Number.\n");
+        printf("End Program.");
+    }
+   
     return 0;
 }
